cad4378c247474b21cb9956ff9577973a3616144
KieranTheMan <kieran9fowler@gmail.com>
2021-03-04 12:43:05 +0000

    get accounts, creat helper function to allocate token to address

--- test/dex.js
+++ test/dex.js
@@ -4,8 +4,10 @@ const Rep = artifacts.require('mocks/Rep.sol');
 const Zrx = artifacts.require('mocks/Zrx.sol');
 const Dex = artifacts.require('Dex.sol');
 
-contract('Dex', ()=> {
+contract('Dex', (accounts)=> {
     let dai, bat, rep, zrx;
+    const [trader1, trader2] = [accounts[1], accounts[2]];
+
     const [DAI, BAT, REP, ZRX] = ['DAI','BAT','REP','ZRX',].map(ticker => web3.utils.fromAscii('ticker'));
 
     beforeEach(async() => {
@@ -22,5 +24,24 @@ contract('Dex', ()=> {
             dex.addToken(RAP, rep.address),
             dex.addToken(ZRX, zrx.address),
         ]);
+
+        //allocate initial token balance
+        const amount = web3.utils.toWei('1000');//
+        //function to allocate ECS20 token
+        const seedTokenBalance = async (token, trader) => {
+            await token.faucet(transfer, amount);//allocate token to a address
+            await token.approve(
+                dex.address,
+                amount,
+                {from: trader}
+            );
+        };
+
+        await Promise.all(
+            [dai, bat, rep, zrx].map(token => seedTokenBalance(token, trader1))
+        );
+        await Promise.all(
+            [dai, bat, rep, zrx].map(token => seedTokenBalance(token, trader2))
+        );
     });
 });
