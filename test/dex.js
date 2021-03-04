e9ad8e62a103340f6874486b17fe078ef7ba1f42
KieranTheMan <kieran9fowler@gmail.com>
2021-03-04 10:51:19 +0000

    include Dex artifacts, web3 fromAscii for tickers and addToken function for testing

--- test/dex.js
+++ test/dex.js
@@ -2,9 +2,11 @@ const Dai = artifacts.require('mocks/Dai.sol');
 const Bat = artifacts.require('mocks/Bat.sol');
 const Rep = artifacts.require('mocks/Rep.sol');
 const Zrx = artifacts.require('mocks/Zrx.sol');
+const Dex = artifacts.require('Dex.sol');
 
 contract('Dex', ()=> {
     let dai, bat, rep, zrx;
+    const [DAI, BAT, REP, ZRX] = ['DAI','BAT','REP','ZRX',].map(ticker => web3.utils.fromAscii('ticker'));
 
     beforeEach(async() => {
         ([dai, bat, rep, zrx] = await Promise.all([
@@ -13,5 +15,12 @@ contract('Dex', ()=> {
             Rep.new(),
             Zrx.new()
         ]));
+        const dex = await Dex.new();
+        await Promise.all([
+            dex.addToken(DAI, dai.address),
+            dex.addToken(BAT, bat.address),
+            dex.addToken(RAP, rep.address),
+            dex.addToken(ZRX, zrx.address),
+        ]);
     });
 });
