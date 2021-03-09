1eeb67c79fcb93aa395972b318551a380334c7f2
KieranTheMan <kieran9fowler@gmail.com>
2021-03-09 10:35:44 +0000

    add two Tests for deposit function

--- test/dex.js
+++ test/dex.js
@@ -1,47 +1,70 @@
+const { expectRevert } = require('@openzeppelin/test-helpers');
 const Dai = artifacts.require('mocks/Dai.sol');
 const Bat = artifacts.require('mocks/Bat.sol');
 const Rep = artifacts.require('mocks/Rep.sol');
 const Zrx = artifacts.require('mocks/Zrx.sol');
 const Dex = artifacts.require('Dex.sol');
 
-contract('Dex', (accounts)=> {
-    let dai, bat, rep, zrx;
-    const [trader1, trader2] = [accounts[1], accounts[2]];
-
-    const [DAI, BAT, REP, ZRX] = ['DAI','BAT','REP','ZRX',].map(ticker => web3.utils.fromAscii('ticker'));
+contract('Dex', (accounts) => {
+    let dai, bat, rep, zrx, dex;
+    const [trader1, trader2] = [accounts[1], accounts[2]];// extract addresses from Dex
+    const [DAI, BAT, REP, ZRX] = ['DAI','BAT','REP','ZRX'].map(ticker => web3.utils.fromAscii('ticker'));
 
     beforeEach(async() => {
-        ([dai, bat, rep, zrx] = await Promise.all([
-            Dai.new(),
-            Bat.new(),
-            Rep.new(),
-            Zrx.new()
-        ]));
-        const dex = await Dex.new();
-        await Promise.all([
-            dex.addToken(DAI, dai.address),
-            dex.addToken(BAT, bat.address),
-            dex.addToken(RAP, rep.address),
-            dex.addToken(ZRX, zrx.address),
-        ]);
+            ([dai, bat, rep, zrx] = await Promise.all([
+                Dai.new(),
+                Bat.new(),
+                Rep.new(),
+                Zrx.new()
+            ]));
+            dex = await Dex.new();
+            await Promise.all([
+                dex.addToken(DAI, dai.address),
+                dex.addToken(BAT, bat.address),
+                dex.addToken(REP, rep.address),
+                dex.addToken(ZRX, zrx.address),
+            ]);
 
-        //allocate initial token balance
-        const amount = web3.utils.toWei('1000');//
-        //function to allocate ECS20 token
-        const seedTokenBalance = async (token, trader) => {
-            await token.faucet(transfer, amount);//allocate token to a address
-            await token.approve(
-                dex.address,
-                amount,
-                {from: trader}
-            );
-        };
+            //allocate initial token balance
+            const amount = web3.utils.toWei('1000');// 1 = 10 ^ 18 cents of token//convert ether to Wei
+            //fhelper unction to allocate ECS20 token
+            const seedTokenBalance = async (token, trader) => {
+                        await token.faucet(trader, amount);//allocate token to a address
+                        await token.approve(
+                        dex.address,
+                        amount,
+                        {from: trader}
+                        );
+            };
 
-        await Promise.all(
-            [dai, bat, rep, zrx].map(token => seedTokenBalance(token, trader1))
-        );
-        await Promise.all(
-            [dai, bat, rep, zrx].map(token => seedTokenBalance(token, trader2))
-        );
+                await Promise.all(
+                [dai, bat, rep, zrx].map(token => seedTokenBalance(token, trader1)));
+                await Promise.all(
+                [dai, bat, rep, zrx].map(token => seedTokenBalance(token, trader2)));
     });
-});
+//happy path
+            it('should deposit tokens', async () => {
+            const amount = web3.utils.toWei('100');
+                await dex.deposit(
+                amount,
+                DAI,
+                {from: trader1}
+                );
+                const balance = await dex.traderBalances(trader1, DAI);
+                assert(balance.toString() === amount);
+            });
+
+//unhappy path
+            it('Should NOT deposit token if token does not exist', async () => {
+    //expectRevert is to test if an error was thrown
+                await expectRevert(
+                    dex.deposit(
+                    web3.utils.toWei('100'),
+                    web3.utils.fromAscii('TOKEN-DOES-NOT-EXIST'),
+                    {from: trader1}
+                    ),
+                    'this token does not exist'
+                );
+            });
+                    
+})
