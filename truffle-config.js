3ee5a8f0ad2ac8463667852da9dc0110a64fae74
KieranTheMan <kieran9fowler@gmail.com>
2021-05-09 21:51:58 +0100

    change version and path name to client

--- truffle-config.js
+++ truffle-config.js
@@ -3,10 +3,10 @@ const path = require("path");
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
-  contracts_build_directory: path.join(__dirname, "clients/src/contracts"),
+  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   compilers: {
     solc: {
-      version: '0.6.3'
+      version: '^0.6.3'
     }
   }
 };
